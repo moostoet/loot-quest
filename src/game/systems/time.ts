c4d33db407a1bf9d14f66e7ba8b329652054b2ca
Moos Toet <moostoet@hotmail.com>
2022-11-19 13:16:47 +0100

    time system stuff

--- src/game/systems/time.ts
+++ src/game/systems/time.ts
@@ -8,6 +8,7 @@ export function createTimeSystem() {
         world.time.delta = delta;
         world.time.elapsed += delta;
         world.time.then = now;
+        console.log(world.time);
         return world;
     });
 }
